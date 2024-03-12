6a28a72857929aa5cb8aafe38a7a51e2d42fc621
Tobias <tobnil93@gmail.com>
2024-03-12 22:02:38 +0100

    (Re)Move to Unity package

--- Interfaces/IAuthenticate.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using App.Scripts.Netcode.Helpers;
-
-namespace App.Scripts.Netcode.Interfaces {
-    public interface IAuthenticate {
-        public void Authenticate(Action<ResultData> callback = null);
-        public Action<ResultData> OnAuthenticateComplete { get; set; }
-    }
-}
--- Interfaces/IUninitialize.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using App.Scripts.Netcode.Helpers;
-
-namespace App.Scripts.Netcode.Interfaces {
-    public interface IUninitialize {
-        public void Uninitialize(Action<ResultData> onComplete = null);
-    }
-}
